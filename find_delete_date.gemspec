5e180b79283018b7ad98cbc1d95115cf4458f08b
Harshvardhan Parihar <harshvardhanparihar@gmail.com>
2021-08-08 19:36:39 +0530

    Added logic for delete date calucation

--- find_delete_date.gemspec
+++ find_delete_date.gemspec
@@ -6,18 +6,18 @@ Gem::Specification.new do |spec|
   spec.name          = "find_delete_date"
   spec.version       = FindDeleteDate::VERSION
   spec.authors       = ["Harshvardhan Parihar"]
-  spec.email         = ["harshvardhan@builder.ai"]
+  spec.email         = ["harshvardhanparihar@gmail.com"]
 
-  spec.summary       = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description   = "TODO: Write a longer description or delete this line."
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "find delete date for create date"
+  spec.description   = "find delete date for create date"
+  spec.homepage      = ""
   spec.required_ruby_version = ">= 2.4.0"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  # spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
 
-  spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  # spec.metadata["homepage_uri"] = spec.homepage
+  # spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
+  # spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
--- lib/find_delete_date.rb
+++ lib/find_delete_date.rb
@@ -1,8 +1,63 @@
 # frozen_string_literal: true
 
 require_relative "find_delete_date/version"
+require 'date'
 
 module FindDeleteDate
   class Error < StandardError; end
-  # Your code goes here...
+
+
+  class Calculate
+    PLANS =  {
+      standard: {
+        days: 42
+      },
+      gold: {
+        days: 42,
+        months: 365
+      },
+      platinum: {
+        days: 42,
+        months: 365,
+        years: 7*365
+      }
+    }
+
+    def self.last_date?(date, end_date)
+      date == end_date
+    end
+
+
+    def self.add_days(name, date, duration_type)
+      date + PLANS[name][duration_type]
+    end
+
+    def self.delete_date(name, date)
+      delete_date = nil
+      day = date.day
+      month = date.month
+      year = date.year
+      month_end = Date.civil(year, month, -1)
+      year_end = Date.civil(year, -1, -1)
+
+      case name
+      when :standard
+        delete_date = add_days(name, date, :days)
+      when :gold
+        delete_date = last_date?(date, month_end) ? add_days(name, date, :months) : add_days(name, date, :days)
+      when :platinum
+        delete_date = if last_date?(date, year_end)
+                        add_days(name, date, :years)
+                      elsif last_date?(date, month_end)
+                        add_days(name, date, :months)
+                      else
+                        add_days(name, date, :days)
+                      end
+      else
+        puts "Please provide Plan name"
+      end
+      delete_date
+    end
+  end
+
 end
